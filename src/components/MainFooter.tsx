64ea16b7fe2569473ac9de69820a4628e5023c85
Elijah <Elijah@Jesuskids-iMac.local>
2024-07-10 22:25:12 -0400

    Updated footer with navigation options

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import { IonFooter, IonToolbar, IonTitle, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
+import { personAddOutline, peopleOutline, schoolOutline, addCircleOutline, logOutOutline, homeOutline } from 'ionicons/icons';
 import './MainFooter.css';
-import { IonContent, IonFooter, IonTitle, IonToolbar } from '@ionic/react';
 
-function MainFooter() {
+const MainFooter: React.FC = () => {
   return (
     <>
       <IonFooter>
-        <IonToolbar color='background'>
-          <IonTitle className='footer-title'>©Nabia Fellows 2024</IonTitle>
-        </IonToolbar>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="add-member" href="/add-member">
+            <IonIcon icon={personAddOutline} />
+            <IonLabel>Add Member</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="member-list" href="/member-list">
+            <IonIcon icon={peopleOutline} />
+            <IonLabel>Member List</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="dashboard" href="/dashboard">
+            <IonIcon icon={homeOutline} />
+            <IonLabel>Dashboard</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tribe-list" href="/tribe-list">
+            <IonIcon icon={schoolOutline} />
+            <IonLabel>View Tribes</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="club-page" href="/club-page">
+            <IonIcon icon={addCircleOutline} />
+            <IonLabel>Create/View Club</IonLabel>
+          </IonTabButton>
+          
+        </IonTabBar>
       </IonFooter>
     </>
   );
-}
+};
+
 export default MainFooter;
